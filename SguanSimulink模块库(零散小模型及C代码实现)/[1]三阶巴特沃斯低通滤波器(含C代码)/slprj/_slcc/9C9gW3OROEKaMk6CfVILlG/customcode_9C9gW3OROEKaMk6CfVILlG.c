25e16f6dcc721145bf51e0d3e43ebf3bf945ba9c
星必尘Sguan <148698322+Sguan-ZhouQing@users.noreply.github.com>
2026-01-22 12:36:56 +0800

    Add files via upload

---
+++ SguanSimulink模块库(零散小模型及C代码实现)/[1]三阶巴特沃斯低通滤波器(含C代码)/slprj/_slcc/9C9gW3OROEKaMk6CfVILlG/customcode_9C9gW3OROEKaMk6CfVILlG.c
@@ -0,0 +1,9 @@
+#include "customcode_9C9gW3OROEKaMk6CfVILlG.h"
+
+/* Function Definition */
+void customcode_9C9gW3OROEKaMk6CfVILlG_initializer(void)
+{
+   BPF_Init();
+}
+
+
---
+++ SguanSimulink模块库(零散小模型及C代码实现)/[1]三阶巴特沃斯低通滤波器(含C代码)/slprj/_slcc/9C9gW3OROEKaMk6CfVILlG/slcc_interface_9C9gW3OROEKaMk6CfVILlG.h
@@ -0,0 +1,25 @@
+#include "customcode_9C9gW3OROEKaMk6CfVILlG.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* Type Definitions */
+
+/* Named Constants */
+
+/* Variable Declarations */
+
+/* Variable Definitions */
+
+/* Function Declarations */
+DLL_EXPORT_CC extern const char_T *get_dll_checksum_9C9gW3OROEKaMk6CfVILlG(void);
+DLL_EXPORT_CC extern void BPF_Init_9C9gW3OROEKaMk6CfVILlG(void);
+DLL_EXPORT_CC extern void BPF_Loop_9C9gW3OROEKaMk6CfVILlG(real32_T input, real32_T *output);
+
+/* Function Definitions */
+DLL_EXPORT_CC const uint8_T *get_checksum_source_info(int32_T *size);
+#ifdef __cplusplus
+}
+#endif
+
